ce54408b8f8bfd203648af425e2feb0daaad890c
guohao <guohao@topwisesz.com>
2018-09-30 10:37:51 +0800

    init commit

---
+++ app/src/main/java/com/topwise/jnidemo/MainActivity.java
@@ -0,0 +1,12 @@
+package com.topwise.jnidemo;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseTestActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
